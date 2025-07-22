49325433a32fc7e9385536960d02a0cf1902ae3d
tomrutherford <tmrutherford@gmail.com>
2025-07-21 23:19:49 -0600

    Basic async

--- src/longmove/core.py
+++ src/longmove/core.py
@@ -2,4 +2,5 @@ import trio
 
 
 async def rsync_copy(src: str, target: str):
+    await trio.run_process(["rsync", "-a", src, target])
     asdf
--- tests/test_core.py
+++ tests/test_core.py
@@ -3,11 +3,27 @@ import pathlib
 import pytest
 import trio
 
+from longmove import core
+
 
 @pytest.fixture
 async def trio_path(tmp_path: pathlib.Path) -> trio.Path:
     return trio.Path(tmp_path)
 
 
-async def test_rsync_copy(tmp_path: trio.Path):
+@pytest.fixture
+async def source_files(trio_path: trio.Path) -> trio.Path:
+    root = trio_path / "root"
+    await root.mkdir()
+    for char in "abcd":
+        f = root / char
+        await f.write_text(char * 100)
+    return root
+
+
+async def test_rsync_copy(trio_path: trio.Path, source_files: trio_path):
+    tgt = trio_path / "target"
+    await tgt.mkdir()
+
+    await core.rsync_copy(source_files, tgt)
     assert 0
