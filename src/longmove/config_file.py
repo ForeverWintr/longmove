ac5e0bea7f4cb63ab7ef6508e156239c2501e40f
tomrutherford <tmrutherford@gmail.com>
2025-09-21 11:19:40 -0600

    Add to/from file

--- src/longmove/config_file.py
+++ src/longmove/config_file.py
@@ -1,15 +1,30 @@
+import typing as tp
+
 import dataclasses
 from pathlib import Path
 
 import tomlkit
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class ConfigFile:
     remote_name: str
-    path_map: tuple[tuple[Path, Path]]
+    path_map: tuple[tuple[Path, Path]] = ()
 
-    def to_yaml(self) -> str:
+    @classmethod
+    def from_toml(cls, toml: str) -> tp.Self:
+        d = dict(tomlkit.parse(toml))
+        d["path_map"] = tuple((Path(a), Path(b)) for a, b in d["path_map"])
+        return cls(**d)
+
+    @classmethod
+    def from_file(cls, f: Path) -> tp.Self:
+        return cls.from_toml(f.read_text())
+
+    def to_toml(self) -> str:
         d = dataclasses.asdict(self)
         d["path_map"] = tuple((str(a), str(b)) for a, b in self.path_map)
         return tomlkit.dumps(d)
+
+    def to_file(self, f: Path) -> None:
+        return f.write_text(self.to_toml())
--- tests/test_config_file.py
+++ tests/test_config_file.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from longmove import config_file
 
 
-def test_to_from_yaml() -> None:
+def test_to_from_toml(tmp_path: Path) -> None:
     c = config_file.ConfigFile(
         remote_name="remote.server",
         path_map=(
@@ -12,9 +12,15 @@ def test_to_from_yaml() -> None:
         ),
     )
 
-    y = c.to_yaml()
+    y = c.to_toml()
     assert y == (
         'remote_name = "remote.server"\n'
         'path_map = [["/tmp/foo", "/server/offload/foo"], ["/tmp/foo2", '
         '"/server/offload/foo2"]]\n'
     )
+
+    out = tmp_path / "config.toml"
+    c.to_file(out)
+
+    c2 = config_file.ConfigFile.from_file(out)
+    assert c == c2
