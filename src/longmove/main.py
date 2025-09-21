5bb2297b4cd4928571661022dfd9f6d5ce821ef7
tomrutherford <tmrutherford@gmail.com>
2025-09-21 11:10:28 -0600

    WIP click

--- src/longmove/main.py
+++ src/longmove/main.py
@@ -1,6 +1,17 @@
 import click
 
+from longmove.config_file import ConfigFile
 
-@click.command()
+
+@click.group()
 def main() -> None:
     print("hello")
+
+
+@main.command(name="init")
+@click.option(
+    "--server", prompt="Please enter a server URL", help="The remote server url"
+)
+def init(server: str):
+    ConfigFile(remote_name=server)
+    pass
