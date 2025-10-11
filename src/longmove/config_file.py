20ad1dc776aca09509c51735277c28557a7d284b
tomrutherford <tmrutherford@gmail.com>
2025-10-10 18:42:17 -0600

    Config object needs to track its file

--- src/longmove/config_file.py
+++ src/longmove/config_file.py
@@ -20,21 +20,24 @@ class ConfigFile:
     remote_name: str = ""
     remote_root: str = ""
     path_map: tuple[tuple[Path, Path]] = ()
+    config_location: Path = dataclasses.field(default=get_default_config_path)
 
     @classmethod
-    def from_toml(cls, toml: str) -> tp.Self:
+    def from_toml(cls, toml: str, config_location: Path) -> tp.Self:
         d = dict(tomlkit.parse(toml))
         d["path_map"] = tuple((Path(a), Path(b)) for a, b in d["path_map"])
+        d["config_location"] = config_location
         return cls(**d)
 
     @classmethod
     def from_file(cls, f: Path) -> tp.Self:
-        return cls.from_toml(f.read_text())
+        return cls.from_toml(f.read_text(), config_location=f)
 
     def to_toml(self) -> str:
         d = dataclasses.asdict(self)
+        d.pop("config_location")
         d["path_map"] = tuple((str(a), str(b)) for a, b in self.path_map)
         return tomlkit.dumps(d)
 
-    def to_file(self, f: Path) -> None:
-        f.write_text(self.to_toml())
+    def to_file(self) -> None:
+        self.config_location.write_text(self.to_toml())
--- tests/test_config_file.py
+++ tests/test_config_file.py
@@ -4,6 +4,7 @@ from longmove import config_file
 
 
 def test_to_from_toml(tmp_path: Path) -> None:
+    out = tmp_path / "config.toml"
     c = config_file.ConfigFile(
         remote_name="remote.server",
         remote_root="/offload",
@@ -11,10 +12,10 @@ def test_to_from_toml(tmp_path: Path) -> None:
             (Path("/tmp/foo"), Path("/server/offload/foo")),
             (Path("/tmp/foo2"), Path("/server/offload/foo2")),
         ),
+        config_location=out,
     )
 
-    out = tmp_path / "config.toml"
-    c.to_file(out)
+    c.to_file()
 
     c2 = config_file.ConfigFile.from_file(out)
     assert c == c2
